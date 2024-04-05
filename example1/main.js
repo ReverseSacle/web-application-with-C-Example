9a5b0e5dc41435ba17c02d1172bb940f3b8e7857
ReverseSacle <reversesacle@gmail.com>
2024-04-05 13:03:53 +0800

    revise

--- example1/main.js
+++ example1/main.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow, globalShortcut, ipcMain, nativeTheme } = require('electron/main')
+const { app, BrowserWindow, globalShortcut, ipcMain, nativeTheme, Menu, MenuItem } = require('electron/main')
 const path = require('node:path')
 const fs = require('fs')
 const { load, DataType, open, close } = require('ffi-rs')
+
+const menu = new Menu()
 const cur_dir = __dirname
 const lib_path = path.join(cur_dir,'lib');
 
@@ -24,47 +26,60 @@ function createWindow()
     mainWindow.loadFile('index.html')
 }
 
-ipcMain.handle('dark-mode:toggle', function() {
-    if (nativeTheme.shouldUseDarkColors) {
-        nativeTheme.themeSource = 'light'
-    } else {
-        nativeTheme.themeSource = 'dark'
-    }
+function fileSave()
+{
+    const save_path = path.join(cur_dir,'log')
+    console.log(save_path)
+    if(false == fs.existsSync(save_path)){ fs.mkdir(save_path, { recursive: true },() => {}) }
+
+    mainWindow.webContents.executeJavaScript(`document.getElementById('textblock').value;`)
+    .then(value => {
+        open({
+            library: 'savefile',
+            path: path.join(lib_path,'savefile.dll')
+        })
+        load({
+            library: 'savefile',
+            funcName: 'saveFile',
+            retType: DataType.Void,
+            paramsType: [DataType.String,DataType.String],
+            paramsValue: [path.join(save_path,'test.txt'),value]
+        })
+        close('savefile')
+    })
+}
+
+menu.append(new MenuItem({
+    label: 'File',
+    submenu: [{
+      label: 'SaveFile',
+      accelerator: 'Ctrl+S',
+      click: () => { fileSave() }
+    }]
+}))
+
+Menu.setApplicationMenu(menu)
+
+ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) { nativeTheme.themeSource = 'light' } 
+    else { nativeTheme.themeSource = 'dark' }
+
     return nativeTheme.shouldUseDarkColors
 })
 
-app.whenReady().then(function() {
+app.whenReady().then(() => {
     createWindow()
 
-    globalShortcut.register('CommandOrControl+S', function() {
-        const save_path = path.join(cur_dir,'log')
-        console.log(save_path)
-        if(false == fs.existsSync(save_path)){ fs.mkdir(save_path, { recursive: true },function(){}) }
-    
-        mainWindow.webContents.executeJavaScript(`document.getElementById('textblock').value;`)
-        .then(value => {
-            load({
-                library: 'savefile',
-                funcName: 'saveFile',
-                retType: DataType.Void,
-                paramsType: [DataType.String,DataType.String],
-                paramsValue: [path.join(save_path,'test.txt'),value]
-            })
-        })
-
-    })
-
-    app.on('activate', function() {
+    app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
     })
 })
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') 
     {
-        close('savefile')
         app.quit()
     }
 })
